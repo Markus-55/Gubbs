d86fdb8dc92d131f5dc10a92e190511fd3f602c2
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2026-09-11 14:32:10 +0200

    Add show and hide contact button

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function hideContact() {
+  let hideContact = document.getElementById("hide-contact");
 
+  if (hideContact.style.display === "block") {
+    hideContact.style.display = "none";
+  } else {
+    hideContact.style.display = "block";
+  }
+}

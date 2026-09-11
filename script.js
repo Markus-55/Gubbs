c0de44098b0577943f077948c3e437e153c392c8
Robin Wedin <robin.wedin@outlook.com>
2026-09-11 14:50:18 +0200

    La till pappa skämt

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function hideContact() {
     hideContact.style.display = "block";
   }
 }
+       let citat = [
+            "Fryser du?  Då kan du ställa dig i hörnet, där är det 90 grader.",
+            "Vad kallar man en praktikant på McDonalds? – En nyburgare.",
+            "Vad gör tandläkaren på lunchen? – Käkar.",
+            "Varför blev du kirurg? – Bra snittlön.",
+            
+
+        ];
+
+        function Joke() {
+            let index = Math.floor(Math.random() * citat.length);
+
+            document.getElementById("citat").textContent = citat[index];
+        }
